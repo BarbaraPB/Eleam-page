b6ab63aeddb57940ea4995c66aacaa9d9b55fd12
Tomás Vélez Núñez <43381969+txvnt@users.noreply.github.com>
2020-01-11 19:11:10 -0300

    Update CV.js

--- src/components/CV.js
+++ src/components/CV.js
@@ -15,11 +15,13 @@ export default class Curriculum extends Component {
                         <h5>ESTAMOS CRECIENDO ¿QUIERES SER PARTE DE NUESTRO EQUIPO?</h5>
                         <h4>Buscamos personas responsables, con gran motivación y compromiso que apuesten por el trabajo en equipo para lograr un objetivo común. Así que si quieres unirte a nuestro equipo y te identificas con nuestro proposito, envíanos tu CV.</h4>
                     </div>
+                    {/*
                     <div className='cv-button'>
                         <Link to='/curriculum'>
                             <button><h6>VER MÁS</h6></button>
                         </Link>
                     </div>
+                    */}
                 </div>
             </div>
         )
